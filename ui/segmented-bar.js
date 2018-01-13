bbfb2574cfd799f587d52fbdcc0832fb9d175501
MMB2 <git@mmb2.click>
2018-01-13 10:33:33 +0100

    :page_facing_up: add segmented-bar mock

---
+++ ui/segmented-bar.js
@@ -0,0 +1,11 @@
+const BaseComponent = require("../base")
+
+class SegmentedBar extends BaseComponent {
+}
+
+class SegmentedBarItem extends BaseComponent {
+}
+
+module.exports = {
+  SegmentedBar, SegmentedBarItem
+}
